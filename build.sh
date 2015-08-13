df6637145516d3004e6f9a7db0d2350a07d023fb
James Lee <jin.li@nufront.com>
2015-08-13 22:47:49 +0800

    change to openwrt cross compile

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 TOP_DIR=$(pwd)
 
-DEF_GCC=arm-none-linux-gnueabi-gcc
-DEF_GXX=arm-none-linux-gnueabi-g++
+DEF_GCC=arm-openwrt-linux-gnueabi-gcc
+DEF_GXX=arm-openwrt-linux-gnueabi-g++
 
 OPENSSL_NAME=openssl
 OPENSSL_VER=1.0.2d
@@ -19,30 +19,8 @@ function compile_zlib()
 	tar zxf zlib-1.2.8.tar.gz
 	cd ./zlib-1.2.8
 	echo "Enter $(pwd)"
-	./configure --prefix=/system
+	./configure --prefix=/system_sec --static
 	sed -i "s/gcc/$DEF_GCC/g" Makefile
-	make -j3 && make install
-}
-
-function compile_openssl()
-{
-	cd $TOP_DIR
-	rm -rf ./openssl-1.0.2d
-	tar zxf  openssl-1.0.2d.tar.gz
-	cd ./openssl-1.0.2d/
-	echo "Enter $(pwd)"
-	./Configure android-armv7 --prefix=/system/  CC=arm-none-linux-gnueabi-gcc CXX=arm-none-linux-gnueabi-g++
-	#./config android-armv7 --prefix=/system/  CC=arm-none-linux-gnueabi-gcc CXX=arm-none-linux-gnueabi-g++
-	sed -i 's/CC=\ gcc/CC=\ arm-none-linux-gnueabi-gcc/g' Makefile
-	sed -i 's/CC=\ cc/CC=\ arm-none-linux-gnueabi-gcc/g' Makefile
-	sed -i 's/\-mandroid//g' Makefile
-	sed -i 's/LD_LIBRARY_PATH=/#LD_LIBRARY_PATH=/g' Makefile
-	sed -i 's/\/usr/\/system\/usr/g' tools/c_rehash
-	sed -i '/MAKEFILE=/a\INSTALL_PREFIX=\/system' tools/Makefile
-	#sed -i "s/all\ install_docs\ install_sw/all\ install_sw/g" Makefile
-	#find -name Makefile|sed -i '/MAKEFILE=/a\INSTALL_PREFIX=\/system'
-	find -name Makefile|sed -i 's/$(INSTALL_PREFIX)/\/system/g'
-	sed -i 's/$(INSTALL_PREFIX)/\/system/g' Makefile
 	make -j3 && make install
 }
 
@@ -54,7 +32,7 @@ function compile_openssl()
 	#cd ./lzma-4.65/
 	#echo "Enter $(pwd)"
 	#cd ./C/LzmaUtil
-	#sed -i 's/CXX\ =\ g++/CXX\ =\ arm-none-linux-gnueabi-g++/g' makefile.gcc
+	#sed -i 's/CXX\ =\ g++/CXX\ =\ arm-openwrt-linux-gnueabi-g++/g' makefile.gcc
 	#make -f makefile.gcc
 	##make && make install
 #}
@@ -67,7 +45,7 @@ function compile_libxml2()
 	tar zxf libxml2-2.9.2.tar.gz
 	cd ./libxml2-2.9.2/
 	echo "Enter $(pwd)"
-	./configure --prefix=/system --host=arm-none-linux-gnueabi CC=arm-none-linux-gnueabi-gcc CXX=arm-none-linux-gnueabi-g++ --target=arm
+	./configure --prefix=/system_sec --host=arm-openwrt-linux-gnueabi CC=arm-openwrt-linux-gnueabi-gcc CXX=arm-openwrt-linux-gnueabi-g++ --target=arm --enable-static=yes
 	sed -i "s/\-llzma//g" Makefile
 	#sed -i "s/LZMA_LIBS/#LZMA_LIBS/g" Makefile
 	sed -i "s/PYTHON\ =/#PYTHON\ =/g" Makefile
@@ -89,7 +67,7 @@ function compile_php5()
 	tar jxf php-5.4.27.tar.bz2
 	cd ./php-5.4.27/
 	echo "Enter $(pwd)"
-	./configure --prefix=/system --host=arm-none-linux-gnueabi CC=arm-none-linux-gnueabi-gcc CXX=arm-none-linux-gnueabi-g++ --target=arm --disable-all
+	./configure --prefix=/system_sec --host=arm-openwrt-linux-gnueabi CC=arm-openwrt-linux-gnueabi-gcc CXX=arm-openwrt-linux-gnueabi-g++ --target=arm --disable-all --enable-static=yes
 	make -j3 && make install
 }
 
@@ -105,7 +83,7 @@ function compile_httpd()
 	#patch -p1 < 001-Makefile_in.patch
 	echo "Enter $(pwd)"
 	sed -i "s/ap_cv_void_ptr_lt_long=yes/ap_cv_void_ptr_lt_long=no/g" configure
-	./configure --prefix=/system --host=arm-none-linux-gnueabi CC=arm-none-linux-gnueabi-gcc CXX=arm-none-linux-gnueabi-g++
+	./configure --prefix=/system_sec --host=arm-openwrt-linux-gnueabi CC=arm-openwrt-linux-gnueabi-gcc CXX=arm-openwrt-linux-gnueabi-g++
 	make -j3 && make install
 }
 
@@ -117,9 +95,34 @@ function compile_pcre()
 	tar jxf pcre-$VER.tar.bz2
 	cd ./pcre-$VER/
 	echo "Enter $(pwd)"
-	./configure --prefix=/system --host=arm-none-linux-gnueabi CC=arm-none-linux-gnueabi-gcc CXX=arm-none-linux-gnueabi-g++ --target=arm
+	./configure --prefix=/system_sec --host=arm-openwrt-linux-gnueabi CC=arm-openwrt-linux-gnueabi-gcc CXX=arm-openwrt-linux-gnueabi-g++ --target=arm --enable-static=yes
 	make -j3 && make install
 }
+
+
+function compile_openssl()
+{
+	cd $TOP_DIR
+	rm -rf ./openssl-1.0.2d
+	tar zxf  openssl-1.0.2d.tar.gz
+	cd ./openssl-1.0.2d/
+	echo "Enter $(pwd)"
+	./Configure android-armv7 --prefix=/system_sec/ no-asm shared CC=arm-openwrt-linux-gnueabi-gcc CXX=arm-openwrt-linux-gnueabi-g++
+	#./config android-armv7 --prefix=/system_sec/  CC=arm-openwrt-linux-gnueabi-gcc CXX=arm-openwrt-linux-gnueabi-g++
+	sed -i 's/CC=\ gcc/CC=\ arm-openwrt-linux-gnueabi-gcc/g' Makefile
+	sed -i 's/CC=\ cc/CC=\ arm-openwrt-linux-gnueabi-gcc/g' Makefile
+	sed -i 's/\-mandroid//g' Makefile
+	sed -i 's/LD_LIBRARY_PATH=/#LD_LIBRARY_PATH=/g' Makefile
+	sed -i 's/\/usr/\/system_sec\/usr/g' tools/c_rehash
+	sed -i '/MAKEFILE=/a\INSTALL_PREFIX=\/system_sec' tools/Makefile
+	#sed -i "s/all\ install_docs\ install_sw/all\ install_sw/g" Makefile
+	#find -name Makefile|sed -i '/MAKEFILE=/a\INSTALL_PREFIX=\/system_sec'
+	find -name Makefile|sed -i 's/$(INSTALL_PREFIX)/\/system_sec/g'
+	sed -i 's/$(INSTALL_PREFIX)/\/system_sec/g' Makefile
+	make -j3 && make install
+}
+
+
 function compile_nginx()
 {
 	cd $TOP_DIR
@@ -137,16 +140,15 @@ function compile_nginx()
 	patch -p1 < 101-feature_test_fix.patch
 	patch -p1 < 102-sizeof_test_fix.patch
 	patch -p1 < 103-sys_nerr.patch
-
-	#sed -i "s/\$OPENSSL\/\.openssl/\/system\/usr\/local\/ssl/g" ./auto/lib/openssl/conf
-	#sed -i "s/\$OPENSSL\/\.openssl/\/system\/usr\/local\/ssl/g" ./auto/lib/openssl/make
+	patch -p1 < 200-config.patch
+	patch -p1 < 300-crosscompile_ccflags.patch
+	patch -p1 < 400-nginx-1.4.x_proxy_protocol_patch_v2.patch
+	patch -p1 < 401-nginx-1.4.0-syslog.patch
 
 ./configure --with-ipv6 \
 	--with-http_stub_status_module \
 	--with-http_flv_module  \
-	--with-http_ssl_module \
 	--with-http_dav_module \
-	--prefix=/usr  \
 	--conf-path=/etc/nginx/nginx.conf  \
 	--error-log-path=/var/log/nginx/error.log  \
 	--lock-path=/var/lock/nginx.lock \
@@ -155,16 +157,26 @@ function compile_nginx()
 	--http-proxy-temp-path=/var/lib/nginx/proxy \
 	--http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
 	--without-http_rewrite_module \
-	--with-cc=arm-none-linux-gnueabi-gcc \
+	--with-cc=arm-openwrt-linux-gnueabi-gcc \
 	--crossbuild=Linux::arm  \
 	--http-fastcgi-temp-path=/var/lib/nginx/fastcgi \
-	--with-openssl=$TOP_DIR/openssl-1.0.2d \
+	--prefix=/usr  \
 	--with-zlib=$TOP_DIR/zlib-1.2.8
 
+	#--with-http_ssl_module \
+	#--with-openssl=$TOP_DIR/openssl-1.0.2d \
 ### for openssl ###
-	sed -i 's/no-threads/no-threads\ CC=arm-none-linux-gnueabi-gcc\ CXX=arm-none-linux-gnueabi-g++/g' objs/Makefile
-	#sed -i 's/CC=\ cc/CC=\ arm-none-linux-gnueabi-gcc/g' objs/Makefile
+	#sed -i 's/\.\/config\ /\.\/Configure android-armv7\ CC=arm-openwrt-linux-gnueabi-gcc\ CXX=arm-openwrt-linux-gnueabi-g++\ /g' objs/Makefile
+	#sed -i '/Configure\ /a\\t%% sed -i '\''s\/CC=\\\ cc\/CC=\\\ arm-openwrt-linux-gnueabi-gcc\/g'\''\ Makefile\ \\'  objs/Makefile
+	#sed -i 's/\-mandroid//g' Makefile
+	#sed -i 's/LD_LIBRARY_PATH=/#LD_LIBRARY_PATH=/g' Makefile
+	#sed -i 's/\/usr/\/system_sec\/usr/g' tools/c_rehash
+	#sed -i '/MAKEFILE=/a\INSTALL_PREFIX=\/system_sec' tools/Makefile
+	#find -name Makefile|sed -i 's/$(INSTALL_PREFIX)/\/system_sec/g'
+	#sed -i 's/$(INSTALL_PREFIX)/\/system_sec/g' Makefile
+### end of openssl ###
 
+	sed -i '5 a\DESTDIR=\/system_sec' objs/Makefile
 
 	make -j3 && make install
 }
@@ -177,19 +189,17 @@ echo "start compile ..."
 echo "TOP Dir is $TOP_DIR"
 echo "using $DEF_GCC"
 #################### OK ###################
-#compile_zlib
-#compile_openssl
-#compile_libxml2
-#compile_php5
-#compile_pcre
+compile_zlib
+compile_openssl
+compile_libxml2
+compile_pcre
+compile_php5
 
 
 #################### NG ###################
-#compile_nginx
+compile_nginx
 #compile_httpd
 
-
-#compile_openssl
 
 
 
